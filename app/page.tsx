c8873c63e11340e957348d51211dd3ae8be3227c
haris-khandre <harishkhandre2901@gmail.com>
2024-05-12 11:17:36 +0530

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import BlogSection from "@/components/hospital-card";
 export default function Home() {
   return (
     <div>
-      Hospital images and frontend
-      <div>
         <BlogSection />
-      </div>
     </div>
   );
 }
